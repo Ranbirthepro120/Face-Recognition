def488bb6a6a983c545376c48b86d310a73361c2
Ranbirthepro120 <75839328+Ranbirthepro120@users.noreply.github.com>
2022-08-08 17:33:15 +0500

    Update main.js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function take_snapshot()
     });
 }
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/TBYS-gwkh/", modelLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/TBYS-gwkh/model.json", modelLoaded);
 
 function modelLoaded()
 {
